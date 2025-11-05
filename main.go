fd47e6157e79ec155e40f98881027670c5de4e00
theamandadavis <davisamanda599@gmail.com>
2025-11-05 11:36:11 -0500

    weather app demo

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "weather-app/client"
+
+// Write an HTTP server that serves the forecasted weather. Your server should expose an endpoint that:
+// 1.     Accepts latitude and longitude coordinates
+// 2.     Returns the short forecast for that area for Today (“Partly Cloudy” etc)
+// 3.     Returns a characterization of whether the temperature is “hot”, “cold”, or “moderate” (use your discretion on mapping temperatures to each type)
+// 4.     Use the National Weather Service API Web Service as a data source.
+// The purpose of this exercise is to provide a sample of your work that we can discuss together in the Technical Interview.
+// •         We respect your time. Spend as long as you need, but we intend it to take around an hour.
+// •         We do not expect a production-ready service, but you might want to comment on your shortcuts.
+// •         The submitted project should build and have brief instructions so we can verify that it works.
+// •         The Coding Project should be written in the language for the job you’re applying for. (golang)
+
+func main() {
+	var resp *client.WeatherResponse
+	var err error
+	if resp, err = client.GetWeatherData(39.7456, -97.0892); err != nil {
+		panic(err)
+	}
+	println("Name: " + resp.Name)
+	println("Short Forecast: " + resp.ShortForecast)
+	println("Temperature Characterization: " + resp.TempCharacter)
+
+}
